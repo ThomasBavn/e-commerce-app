afb4dc9e65407ef22de20dbae8ace0f380de6901
Thomas Bavnhøj <thomas.bavnhoj@gmail.com>
2024-06-08 03:01:13 +0200

    make text use typography styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,9 @@ export default async function Home() {
         </H3>
 
         <div className="flex flex-col items-center justify-center gap-4">
-          <P className="text-center text-2xl text-white">
-            {session && <span>Logged in as {session.user?.name}</span>}
-          </P>
+          {session && <P>Logged in as {session.user?.name}</P>}
           <Button asChild>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
+            <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
               {session ? "Sign out" : "Sign in"}
             </Link>
           </Button>
