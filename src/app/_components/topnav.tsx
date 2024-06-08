a330a12d8412ed50161e566437608340348040ed
Thomas Bavnhøj <thomas.bavnhoj@gmail.com>
2024-06-08 03:12:21 +0200

    add shad navigation menu

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "./ui/navigation-menu";
+
+function TopNav() {
+  return (
+    <nav className="flex justify-between">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+}
+
+export default TopNav;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import TopNav from "@components/topnav";
 
 export const metadata = {
   title: "Create T3 App",
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <div className="grid h-screen grid-rows-[auto,1fr]">
+          <TopNav />
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </div>
       </body>
     </html>
   );
